8820fd7e8ee5867f0e50b8731849243607c81041
pmelet <melet.pierre.etienne@gmail.com>
2024-06-20 14:42:06 +0200

    add widget

--- server.py
+++ server.py
@@ -23,29 +23,5 @@ def production():
     d = {"production": prod}  
     return json.dumps(d).encode('utf8')
 
-class PointsRequestHandler (BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
-        points = get_points()
-        prod = []
-        for p in points:
-            prod.append(p.to_dict())
-        d = {"production": prod}  
-        json_as_str = json.dumps(d).encode('utf8')
-        print (json_as_str)
-        self.wfile.write(bytes(json_as_str))
-
-def run():
-    server_address = ('', 8000)
-    httpd = HTTPServer(server_address, PointsRequestHandler)
-    httpd.serve_forever()
-
-#if __name__ == "__main__":
-#    run()
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=False)
