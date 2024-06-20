683bd6d0f6449afd496289893df636bf2ec261c2
pmelet <melet.pierre.etienne@gmail.com>
2024-06-20 11:49:36 +0200

    set url for website

--- server.py
+++ server.py
@@ -1,6 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from points import get_points
 import json
+from flask import Flask
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
+
+@app.route('/')
+def index():
+    return send_from_directory('static', "index.html")
+
+@app.route("/production")
+def production():
+    points = get_points()
+    prod = []
+    for p in points:
+        prod.append(p.to_dict())
+    d = {"production": prod}  
+    return json.dumps(d).encode('utf8')
 
 class PointsRequestHandler (BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,6 +43,9 @@ def run():
     httpd = HTTPServer(server_address, PointsRequestHandler)
     httpd.serve_forever()
 
+#if __name__ == "__main__":
+#    run()
 
-if __name__ == "__main__":
-    run()
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)
---
+++ static/index.js
@@ -0,0 +1,42 @@
+
+function render(array){
+
+    data = array.production.map(x => [x.time*1000, x.watts])
+
+    Highcharts.chart('container', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Production',
+            align: 'left'
+        },
+        xAxis: {
+            type: 'datetime',
+            title: {
+                text: 'Time'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Watts'
+            },
+            min: 0
+        },
+        colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
+        series: [{
+            name: 'Production',
+            data: data
+        }]
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    let url = "production"
+    const options = { method: 'GET' };
+    fetch(url, options)
+        .then(res => res.json())
+        .then(out => render(out))
+        .catch(err => { throw err });
+})
+
