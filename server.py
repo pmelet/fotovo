5753376f9a92ef3c87d121800a39b208bb3e47c3
pmelet <melet.pierre.etienne@gmail.com>
2024-06-20 09:39:14 +0200

    do not repeat

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ class PointsRequestHandler (BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
-        self.send_header("Access-Control-Allow-Origin", "*");
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         points = get_points()
         prod = []
