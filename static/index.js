cc0d71424c1abd5c09d880c2b13760408e1b45c3
pmelet <melet.pierre.etienne@gmail.com>
2024-06-21 15:52:31 +0200

    add avergae

--- static/index.js
+++ static/index.js
@@ -1,12 +1,23 @@
+let chart = null;
 
 function render(array){
-
+    if (!chart) {
+        chart = draw()
+    }
     data = array.production.map(x => [x.time*1000, x.watts])
     yesterday = array.yesterday.map(x => [x.time*1000, x.watts])
     avg = array.stats.map(x => [x.time*1000, x.avg])
     min = array.stats.map(x => [x.time*1000, x.min])
     max = array.stats.map(x => [x.time*1000, x.max])
     stats = array.stats.map(x => [x.time*1000, x.min, x.max])
+
+    chart.series[0].update({data: stats});
+    chart.series[1].update({data: avg });
+    chart.series[2].update({data: yesterday });
+    chart.series[3].update({data: data });
+}
+
+function draw(){
 
     Highcharts.setOptions(
         {
@@ -16,7 +27,10 @@ function render(array){
         }
     )
 
-    Highcharts.chart('container', {
+    return Highcharts.chart('container', {
+        chart: {
+            animation: false,
+        },
         title: {
             text: 'Production',
             align: 'left'
@@ -33,11 +47,10 @@ function render(array){
             },
             min: 0
         },
-        colors: ['#6CF', '#aaF', '#aaF', '#aaF'],
         series: [{
             type: 'areasplinerange',
             name: 'Min/Max',
-            data: stats,
+            data: [],
             opacity: .5,
             color: {
                 linearGradient: {
@@ -55,23 +68,41 @@ function render(array){
         },{
             type: 'spline',
             name: "Moyenne",
-            data: avg,
-            dashStyle: "dot",
+            data: [],
+            color: '#aaf',
             marker: { enabled: false }
+        },{
+            type: 'scatter',
+            name: "Hier",
+            data: [],
+            color: '#ccf',
+            marker: {
+                symbol: 'circle',
+                fillColor: '#FFFFFF',
+                enabled: true,
+                radius: 2,
+                lineWidth: 1,
+                lineColor: null
+            }
         },{
             type: 'spline',
             name: 'Production',
-            data: data
+            data: [],
+            marker: { enabled: false },
+            color: 'red',
         }]
     });
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', load)
+
+function load(){
     let url = "production"
     const options = { method: 'GET' };
     fetch(url, options)
         .then(res => res.json())
         .then(out => render(out))
         .catch(err => { throw err });
-})
 
+    setTimeout(load, 30000);
+}
